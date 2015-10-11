00c3b1c1b11eccea784a585f6ee93b4bac92b9b5
Regis Pires Magalhaes <regispiresmag@gmail.com>
2015-10-11 16:11:03 -0300

    maxFailures just 1 now

--- src/main/scala/PageRank2.scala
+++ src/main/scala/PageRank2.scala
@@ -26,7 +26,7 @@ object PageRank2 extends Serializable with Logging {
       conf.setMaster(master)
     }
     conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-    conf.set("spark.task.maxFailures", "128")
+    conf.set("spark.task.maxFailures", "1")
     conf.set("spark.speculation", "false")
     //conf.set("spark.shuffle.manager", "SORT")
     // Compress RDD in both memory and on-disk using the fast Snappy compression codec
@@ -41,16 +41,25 @@ object PageRank2 extends Serializable with Logging {
     conf.set("spark.core.connection.ack.wait.timeout", "180")
     // Attempting to fix "Map output statuses were 10810591 bytes which exceeds spark.akka.frameSize (10485760 bytes)"
     conf.set("spark.akka.frameSize", "42")
+
+    val startTime = System.nanoTime()
     val sc = new SparkContext(conf)
     if (master != null && master.equals("local")) {
       // if master is different from local, use fs.defaultFS property from core-site.xml
       sc.hadoopConfiguration.set("fs.defaultFS", "file:///")
     }
 
+    var endTime = System.nanoTime()    
+    logInfo("Spark context loaded: " + ((endTime - startTime) / 1000000000d))
+
+    
     val cacheType = StorageLevel.MEMORY_AND_DISK_SER
 
     // Only repartition if it's local as otherwise there's large amounts of network communication
     val lines = sc.textFile(src, parts)
+    endTime = System.nanoTime()    
+    logInfo("Get lines: " + ((endTime - startTime) / 1000000000d))
+
     // Remove any lines which are comments
     val filtered_lines = lines.repartition(parts).filter(line => !line.startsWith("#") && !line.startsWith("%"))
     // Remove any lines starting with # and then split according to tabs
@@ -59,20 +68,29 @@ object PageRank2 extends Serializable with Logging {
       //val fields = line.split(" ")
       (fields(0).toLong, fields(1).toLong)
     }.map(e => Edge(e._1, e._2, 0)) //.persist(StorageLevel.DISK_ONLY)
-    val edgeRDD = EdgeRDD.fromEdges(edgeList)
+    endTime = System.nanoTime()    
+    logInfo("Get edge list: " + ((endTime - startTime) / 1000000000d))
 
-    logInfo("Constructing graph...")
+    val edgeRDD = EdgeRDD.fromEdges(edgeList)
+    endTime = System.nanoTime()    
+    logInfo("Get edge RDD: " + ((endTime - startTime) / 1000000000d))
+
     val graph = Graph.fromEdges(edgeRDD, defaultValue = 0, cacheType, cacheType).partitionBy(PartitionStrategy.RandomVertexCut, parts)
     //edgeList.unpersist()
+    endTime = System.nanoTime()    
+    logInfo("Graph loaded: " + ((endTime - startTime) / 1000000000d))
     logInfo("Vertices: " + graph.vertices.count)
     logInfo("Edges: " + graph.edges.count)
 
     // Run PageRank
     val ranks = graph.staticPageRank(numIter = 30).vertices
+    endTime = System.nanoTime()    
+    logInfo("PageRank executed: " + ((endTime - startTime) / 1000000000d))
     // Print PageRank result
     //println(ranks.collect.toSeq.sortBy(_._2).mkString("\n"))
-    logInfo("Outputting the ranked vertices (gzipped)")
     //ranks.saveAsTextFile("/tmp/rankOutGzip/", classOf[GzipCodec])
     ranks.saveAsTextFile("/tmp/scala-output-" + DateUtils.format())
+    endTime = System.nanoTime()    
+    logInfo("Results saved: " + ((endTime - startTime) / 1000000000d))
   }
 }
